63c7c3cc739d34b5f07c5222b0d596dcfcd8cdd5
Casey Duncan <caseyaduncan08@gmail.com>
2020-04-30 22:15:59 -0600

    Moved arguments for chosing epoch quantity, augmentations, and data split to only run if the train argument is selected within in main.py.

--- main.py
+++ main.py
@@ -9,25 +9,25 @@ import csci508_final as csci
 def main():
     args = get_args()
 
-    if args.split_data:
-        train_ratio, valid_ratio, test_ratio = csci.data_split_ratios.interface()
-    else:
-        train_ratio, valid_ratio, test_ratio = csci.data_split_ratios.initialize_with_default_values()
-
-    if args.augment_data:
-        augmentations = csci.data_augmentation.interface()
-    else:
-        augmentations = csci.data_augmentation.initialize_augmentation_dictionary()
-        print("")
-        print("No transforms will be applied to training data by default.")
-        print("")
-
-    if args.epoch_qty:
-        epoch_qty = csci.epoch_quantity.interface()
-    else:
-        epoch_qty = csci.epoch_quantity.initialize_with_default_values()
-
     if args.train:
+        if args.split_data:
+            train_ratio, valid_ratio, test_ratio = csci.data_split_ratios.interface()
+        else:
+            train_ratio, valid_ratio, test_ratio = csci.data_split_ratios.initialize_with_default_values()
+
+        if args.augment_data:
+            augmentations = csci.data_augmentation.interface()
+        else:
+            augmentations = csci.data_augmentation.initialize_augmentation_dictionary()
+            print("")
+            print("No transforms will be applied to training data by default.")
+            print("")
+
+        if args.epoch_qty:
+            epoch_qty = csci.epoch_quantity.interface()
+        else:
+            epoch_qty = csci.epoch_quantity.initialize_with_default_values()
+            
         model_dict, model_file_name = csci.model_selection.interface()
         csci.train_test_model.main(augmentations, model_dict, model_file_name, train_ratio, valid_ratio, test_ratio, epoch_qty)
     else:
